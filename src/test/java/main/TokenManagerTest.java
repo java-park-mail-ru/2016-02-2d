80d3f8be2dde846cea85ff5d879360088d098f84
Paul Maminov <xobotun@ya.ru>
2016-04-03 18:35:13 +0300

    Removed tests of simple getters and setters.

--- src/test/java/main/TokenManagerTest.java
+++ src/test/java/main/TokenManagerTest.java
@@ -1,5 +1,6 @@
 package main;
 
+import constants.Constants;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,9 +9,17 @@ public class TokenManagerTest {
 
     @Test
     public void testGetNewRandomSessionID() throws Exception {
-        final String cookie1 = TokenManager.getNewRandomSessionID();
-        final String cookie2 = TokenManager.getNewRandomSessionID();
+        final String cookie1 = TokenManager.getNewRandomSessionID("random", "data");
+        final String cookie2 = TokenManager.getNewRandomSessionID("for", "different", "cookies");
+        final String cookie1duplicate = TokenManager.getNewRandomSessionID("random", "data");
 
-        assertNotSame(cookie1, cookie2);
+        assertNotEquals(cookie1, cookie2);
+        assertEquals(cookie1, cookie1duplicate);
+    }
+
+    @Test
+    public void testGetSIDStringFromHeaders() throws Exception {
+        assertEquals(Constants.USER_SESSION_ID, TokenManager.getSIDStringFromHeaders(Constants.FunctionalTestMocks.getOkCookieHeaders()));
+        assertEquals(null, TokenManager.getSIDStringFromHeaders(Constants.FunctionalTestMocks.getNoCookieHeaders()));
     }
 }
--- src/test/java/main/database/UserProfileDataTest.java
+++ src/test/java/main/database/UserProfileDataTest.java
@@ -15,58 +15,6 @@ public class UserProfileDataTest {
     }
 
     @Test
-    public void testGetLogin() throws Exception {
-        assertEquals(Constants.USER_LOGIN, user.getLogin());
-    }
-
-    @Test
-    public void testSetLogin() throws Exception {
-        final String newLogin = "aaa";
-        user.setLogin(newLogin);
-
-        assertEquals(newLogin, user.getLogin());
-    }
-
-    @Test
-    public void testGetPassword() throws Exception {
-        assertEquals(Constants.USER_PASSWORD, user.getPassword());
-    }
-
-    @Test
-    public void testSetPassword() throws Exception {
-        final String newPassword = "bbb";
-        user.setPassword(newPassword);
-
-        assertEquals(newPassword, user.getPassword());
-    }
-
-    @Test
-    public void testGetId() throws Exception {
-        assertEquals(Constants.USER_PASSWORD, user.getPassword());
-    }
-
-    @Test
-    public void testSetId() throws Exception {
-        final long newID = 1;
-        user.setId(newID);
-
-        assertEquals(newID, user.getId());
-    }
-
-    @Test
-    public void testGetScore() throws Exception {
-        assertEquals(0, user.getScore());
-    }
-
-    @Test
-    public void testSetScore() throws Exception {
-        final int newScore = 1;
-        user.setScore(newScore);
-
-        assertEquals(newScore, user.getScore());
-    }
-
-    @Test
     public void testHashCode() throws Exception {
         final UserProfileData user2 = new UserProfileData(Constants.USER_LOGIN, Constants.USER_PASSWORD);
 
